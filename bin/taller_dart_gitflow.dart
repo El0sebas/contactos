9f08bd8d474a36a2abe5078ef31c022bf43ce7d3
El0sebas <sebasmmadriga@gmail.com>
2026-08-21 19:39:59 -0500

    feat: initial dart console project setup

---
+++ bin/taller_dart_gitflow.dart
@@ -0,0 +1,5 @@
+import 'package:taller_dart_gitflow/taller_dart_gitflow.dart' as taller_dart_gitflow;
+
+void main(List<String> arguments) {
+  print('Hello world: ${taller_dart_gitflow.calculate()}!');
+}
---
+++ test/taller_dart_gitflow_test.dart
@@ -0,0 +1,8 @@
+import 'package:taller_dart_gitflow/taller_dart_gitflow.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
